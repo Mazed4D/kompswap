e75e2a309113f1881d7ba0c918c025bc7833c6de
Milan Paunović <paunovic.milan@outlook.com>
2023-01-05 17:52:51 +0100

    border color fix

--- screens/Sell/Sell.tsx
+++ screens/Sell/Sell.tsx
@@ -1,27 +1,76 @@
+import * as ImagePicker from 'expo-image-picker';
+import FastImage from 'react-native-fast-image';
+import { Button, IconButton, TouchableRipple } from 'react-native-paper';
 import { Dimensions, Pressable, StyleSheet } from 'react-native';
-import { IconButton } from 'react-native-paper';
 import { useState } from 'react';
 
 import { Text, View } from '../../components/Themed';
 
 export default function Sell() {
-	const [image, setImage] = useState(null);
+	const [image, setImage] = useState<null | string>(null);
 	const size = Dimensions.get('screen').width * 0.9;
+
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 1,
+		});
+
+		console.log(result);
+
+		if (!result.canceled) {
+			setImage(result.assets[0].uri);
+		} else {
+			setImage(null);
+		}
+	};
 
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Sell (BETA/CPU ONLY)</Text>
-			<IconButton
-				mode='outlined'
-				style={{
-					marginVertical: 20,
-					width: size,
-					height: size,
-				}}
-				size={50}
-				icon={'camera-plus'}
-				onPress={() => console.log('pr')}
-			/>
+			{image ? (
+				<View
+					style={{
+						marginVertical: 20,
+						height: size,
+						width: size,
+						overflow: 'hidden',
+						borderRadius: 45,
+						borderWidth: 1,
+						borderColor: 'white',
+					}}
+				>
+					<TouchableRipple
+						onPress={pickImage}
+						android_ripple={{
+							foreground: true,
+						}}
+						style={{
+							height: size,
+							width: size,
+						}}
+					>
+						<FastImage
+							source={{ uri: image }}
+							style={{ height: size, width: size }}
+						/>
+					</TouchableRipple>
+				</View>
+			) : (
+				<IconButton
+					mode='outlined'
+					style={{
+						marginVertical: 20,
+						width: size,
+						height: size,
+					}}
+					size={50}
+					icon={'camera-plus'}
+					onPress={pickImage}
+				/>
+			)}
 		</View>
 	);
 }
