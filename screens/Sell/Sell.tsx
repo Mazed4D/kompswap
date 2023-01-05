3de0b598f70746da346bdd80287d738534644ebe
Milan Paunović <paunovic.milan@outlook.com>
2023-01-05 15:33:21 +0100

    added add image btn

--- screens/Sell/Sell.tsx
+++ screens/Sell/Sell.tsx
@@ -1,12 +1,27 @@
-import { StyleSheet } from 'react-native';
+import { Dimensions, Pressable, StyleSheet } from 'react-native';
+import { IconButton } from 'react-native-paper';
+import { useState } from 'react';
 
 import { Text, View } from '../../components/Themed';
 
 export default function Sell() {
+	const [image, setImage] = useState(null);
+	const size = Dimensions.get('screen').width * 0.9;
+
 	return (
 		<View style={styles.container}>
 			<Text style={styles.title}>Sell (BETA/CPU ONLY)</Text>
-      <
+			<IconButton
+				mode='outlined'
+				style={{
+					marginVertical: 20,
+					width: size,
+					height: size,
+				}}
+				size={50}
+				icon={'camera-plus'}
+				onPress={() => console.log('pr')}
+			/>
 		</View>
 	);
 }
